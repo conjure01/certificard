c6d8fc4279e51633a44ddb248000355b6ecd9465
conjure01 <91496313+conjure01@users.noreply.github.com>
2021-09-29 16:56:25 -0300

    adicionei o arquivo crú dps da imersão dev

---
+++ script.js
@@ -0,0 +1,10 @@
+//<main class= "projetos">
+//    <ul class= "projects-title"> Projects <ul>
+//    <li class= "projects-item"> <a href="https://codepen.io/conjure1/pen/vYZVgZy"> //🃏Super Trunfo </a> </li>
+//    <li class= "projects-item"> <a href="https://codepen.io/conjure1/pen/VwWxrLQ"> //🎥AnimeFlix </a> </li>
+
+// LEMBRAR DO HREF
+
+function mudarTema() {
+  document.body.classList.toggle("dark")
+}
